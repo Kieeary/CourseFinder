653e23797f6a704ab9cbb0d837411c22ae06fe4d
seungyeon17 <oosara811@naver.com>
2022-04-12 01:46:31 +0900

    백승연/ 대카테고리 이미지 변경

--- app/src/main/java/com/example/coursefinder/PlayingRegister.java
+++ app/src/main/java/com/example/coursefinder/PlayingRegister.java
@@ -16,18 +16,18 @@ public class PlayingRegister extends AppCompatActivity {
     GridView gridView4;
 
     int[] categoryImgId = {R.drawable.hansik, R.drawable.ilsik, R.drawable.yangsik, R.drawable.jungsik,
-            R.drawable.bunsik, R.drawable.yasik};
-    String[] categoryNameId = {"한식", "일식", "양식", "중식", "분식", "야식"};
+            R.drawable.meat, R.drawable.bunsik, R.drawable.yasik, R.drawable.seafood, R.drawable.salad};
+    String[] categoryNameId = {"한식", "일식", "양식", "중식", "고기", "분식", "야식", "해산물", "샐러드"};
 
-    int[] cafeCategoryImgId = {R.drawable.cafe, R.drawable.bakery, R.drawable.macaron};
-    String[] cafeCategoryNameId = {"카페", "베이커리", "마카롱"};
+    int[] cafeCategoryImgId = {R.drawable.cafe, R.drawable.bakery, R.drawable.dessert, R.drawable.beer, R.drawable.wine, R.drawable.cocktail};
+    String[] cafeCategoryNameId = {"카페", "베이커리", "디저트", "맥주", "와인", "칵테일"};
 
     int[] gameCategoryImgId = {R.drawable.pcroom, R.drawable.singingroom, R.drawable.escaperoom,
             R.drawable.dangguroom, R.drawable.bowlingroom, R.drawable.boardgameroom};
     String[] gameCategoryNameId = {"PC방", "노래방", "방탈출", "당구장", "볼링장", "보드게임방"};
 
     int[] cultureCategoryImgId ={R.drawable.movie, R.drawable.exhibition, R.drawable.reading};
-    String[] cultureCategoryNameId ={"영화", "전시회", "독서"};
+    String[] cultureCategoryNameId ={"영화", "전시회", "독서", "공연"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
