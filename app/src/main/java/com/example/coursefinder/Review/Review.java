4614fc53aeff508f90f5ad6d156c446d75870f66
seungyeon17 <oosara811@naver.com>
2022-04-18 21:52:01 +0900

    백승연/ java 수정

--- app/src/main/java/com/example/coursefinder/Review/Review.java
+++ app/src/main/java/com/example/coursefinder/Review/Review.java
@@ -1,4 +1,4 @@
-package com.example.coursefinder;
+package com.example.coursefinder.Review;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coursefinder.Course.CourseDetail;
+import com.example.coursefinder.R;
 
 public class Review extends AppCompatActivity {
 
